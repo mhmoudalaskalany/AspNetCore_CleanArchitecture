2914280ee965a53f9863a7bd1c806fc8865e4e4f
mahmoud alaskalany <mahmoud.ragab@omsgd.gov.om>
2020-08-12 11:15:38 +0400

    .

--- BackendCore.Service/Services/Base/ServiceBaseParameter.cs
+++ BackendCore.Service/Services/Base/ServiceBaseParameter.cs
@@ -2,21 +2,29 @@
 using BackendCore.Common.Abstraction.UnitOfWork;
 using BackendCore.Common.Core;
 using BackendCore.Entities.Entities.Base;
+using Microsoft.AspNetCore.Http;
 
 namespace BackendCore.Service.Services.Base
 {
     public class ServiceBaseParameter<T,TKey> : IServiceBaseParameter<T,TKey> where T : BaseEntity<TKey>
     {
 
-        public ServiceBaseParameter(IMapper mapper, IUnitOfWork<T,TKey> unitOfWork, IResponseResult responseResult)
+        public ServiceBaseParameter(
+            IMapper mapper, 
+            IUnitOfWork<T,TKey> unitOfWork, 
+            IResponseResult responseResult,
+            IHttpContextAccessor httpContextAccessor
+            )
         {
             Mapper = mapper;
             UnitOfWork = unitOfWork;
             ResponseResult = responseResult;
+            HttpContextAccessor = httpContextAccessor;
         }
 
         public IMapper Mapper { get; set; }
         public IUnitOfWork<T, TKey> UnitOfWork { get; set; }
         public IResponseResult ResponseResult { get; set; }
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
     }
 }
