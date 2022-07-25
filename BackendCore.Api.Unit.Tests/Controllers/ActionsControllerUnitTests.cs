0133cb2ee1c21dfb321d0b69e38cc6941f90dacd
Mahmoud Ragab <Mhmoudalaskalany@gmail.com>
2022-07-25 14:18:24 +0400

    remove spaces

--- BackendCore.Api.Unit.Tests/Controllers/ActionsControllerUnitTests.cs
+++ BackendCore.Api.Unit.Tests/Controllers/ActionsControllerUnitTests.cs
@@ -15,24 +15,19 @@ namespace BackendCore.Api.Unit.Tests.Controllers
         {
             _actionServiceMock = new Mock<IActionService>();
             Fixture.Register(() => _actionServiceMock.Object);
-
             _controller = new ActionsController(_actionServiceMock.Object);
         }
 
         [Fact]
         public async Task GetActionsAsync_Return_Ok()
         {
-            // AAA
             //Arrange (set up variables 
-
             var result = (IFinalResult)Fixture.Build<Result>().With(p => p.Status, HttpStatusCode.OK).Create();
-
             _actionServiceMock.Setup(x => x.GetActionsAsync())
                 .Returns(Task.FromResult(result));
 
             //Act ( execute the target method )
             var finalResult = await _controller.GetActionsAsync();
-
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, finalResult.Status);
--- BackendCore.Api.Unit.Tests/Controllers/StatusesControllerUnitTests.cs
+++ BackendCore.Api.Unit.Tests/Controllers/StatusesControllerUnitTests.cs
@@ -9,30 +9,25 @@ namespace BackendCore.Api.Unit.Tests.Controllers
 {
     public class StatusesControllerUnitTests : AutoFixtureBase
     {
-        private readonly Mock<IStatusService> _lookupServiceMock;
+        private readonly Mock<IStatusService> _statusServiceMock;
         private readonly StatusesController _controller;
         public StatusesControllerUnitTests()
         {
-            _lookupServiceMock = new Mock<IStatusService>();
-            Fixture.Register(() => _lookupServiceMock.Object);
-
-            _controller = new StatusesController(_lookupServiceMock.Object);
+            _statusServiceMock = new Mock<IStatusService>();
+            Fixture.Register(() => _statusServiceMock.Object);
+            _controller = new StatusesController(_statusServiceMock.Object);
         }
 
         [Fact]
-        public async Task GetStatusesAsync_Return_Ok()
+        public async Task GetActionsAsync_Return_Ok()
         {
-            // AAA
             //Arrange (set up variables 
-
             var result = (IFinalResult)Fixture.Build<Result>().With(p => p.Status, HttpStatusCode.OK).Create();
-
-            _lookupServiceMock.Setup(x => x.GetStatusesAsync())
+            _statusServiceMock.Setup(x => x.GetStatusesAsync())
                 .Returns(Task.FromResult(result));
 
             //Act ( execute the target method )
             var finalResult = await _controller.GetStatusesAsync();
-
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, finalResult.Status);
