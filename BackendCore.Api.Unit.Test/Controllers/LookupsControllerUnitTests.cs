1b6ff24815ec5276d8fc5dff8bca964684dcc370
Mahmoud Ragab <Mhmoudalaskalany@gmail.com>
2022-07-25 11:21:45 +0400

    add unit tests

---
+++ BackendCore.Api.Unit.Test/Controllers/LookupsControllerUnitTests.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using AutoFixture;
+using BackendCore.Api.Controllers.Lookup;
+using BackendCore.Common.Core;
+using BackendCore.Service.Services.Lookups;
+using Moq;
+
+namespace BackendCore.Api.Unit.Test.Controllers
+{
+    public class LookupsControllerUnitTests : AutoFixtureBase
+    {
+        private readonly Mock<ILookupService> _lookupServiceMock;
+        private readonly LookupsController _controller;
+        public LookupsControllerUnitTests()
+        {
+            _lookupServiceMock = new Mock<ILookupService>();
+            Fixture.Register(() => _lookupServiceMock.Object);
+
+            _controller = new LookupsController(_lookupServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetStatusesAsync_Return_Ok()
+        {
+            // AAA
+            //Arrange (set up variables 
+
+            var result = (IFinalResult)Fixture.Build<Result>().With(p => p.Status, HttpStatusCode.OK).Create();
+
+            _lookupServiceMock.Setup(x => x.GetStatusesAsync())
+                .Returns(Task.FromResult(result));
+
+            //Act ( execute the target method )
+            var finalResult = await _controller.GetStatusesAsync();
+
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, finalResult.Status);
+        }
+
+
+        [Fact]
+        public async Task GetActionsAsync_Return_Ok()
+        {
+            // AAA
+            //Arrange (set up variables 
+
+            var result = (IFinalResult)Fixture.Build<Result>().With(p => p.Status, HttpStatusCode.OK).Create();
+
+            _lookupServiceMock.Setup(x => x.GetActionsAsync())
+                .Returns(Task.FromResult(result));
+
+            //Act ( execute the target method )
+            var finalResult = await _controller.GetActionsAsync();
+
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, finalResult.Status);
+        }
+
+        
+    }
+}
--- BackendCore.Service/Services/Lookups/LookupService.cs
+++ BackendCore.Service/Services/Lookups/LookupService.cs
@@ -11,31 +11,19 @@ namespace BackendCore.Service.Services.Lookups
 {
     public class LookupService : BaseService<Status, AddStatusDto, StatusDto, long, long?>, ILookupService
     {
-        #region Constructors
-
+        
         public LookupService(IServiceBaseParameter<Status> parameters) : base(parameters)
         {
         }
 
-        #endregion
 
-
-        #region Public Methods
-
-        /// <summary>
-        /// Get Statuses
-        /// </summary>
-        /// <returns></returns>
         public async Task<IFinalResult> GetStatusesAsync()
         {
             var entities = await UnitOfWork.Repository.FindAsync(x => x.IsDeleted == false);
             var data = Mapper.Map<IEnumerable<Status>, List<StatusDto>>(entities);
             return new ResponseResult(data, HttpStatusCode.OK, null, "Success");
         }
-        /// <summary>
-        /// Get Actions
-        /// </summary>
-        /// <returns></returns>
+        
         public async Task<IFinalResult> GetActionsAsync()
         {
             var entities = await UnitOfWork.GetRepository<Action>().FindAsync(x => x.IsDeleted == false);
@@ -43,13 +31,7 @@ namespace BackendCore.Service.Services.Lookups
             return new ResponseResult(data, HttpStatusCode.OK, null, "Success");
         }
 
-        #endregion
 
-        #region Private Methods
-
-        
-
-        #endregion
 
 
 
