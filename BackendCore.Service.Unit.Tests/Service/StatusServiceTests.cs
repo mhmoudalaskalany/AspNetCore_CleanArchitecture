c222074920b86dc2f13625a48ceb3fa2696d4389
Mahmoud Alaskalany <mhmoudalaskalany@gmail.com>
2022-07-26 09:02:07 +0400

    .

--- BackendCore.Service.Unit.Tests/Service/StatusServiceTests.cs
+++ BackendCore.Service.Unit.Tests/Service/StatusServiceTests.cs
@@ -1,6 +1,9 @@
 ﻿using System.Linq.Expressions;
+using System.Net;
 using AutoFixture;
+using AutoMapper;
 using BackendCore.Common.Core;
+using BackendCore.Common.DTO.Lookup.Status;
 using BackendCore.Common.Extensions;
 using BackendCore.Common.Infrastructure.Repository;
 using BackendCore.Common.Infrastructure.UnitOfWork;
@@ -14,25 +17,36 @@ namespace BackendCore.Service.Unit.Tests.Service
     public class StatusServiceTests : AutoFixtureBase
     {
         private readonly Mock<IUnitOfWork<Status>> _uowMock;
-        private readonly Mock<IRepository<Status>> _repoMock;
         private readonly Mock<StatusService> _statusServiceMock;
+        private readonly Mock<IMapper> _mapperMock;
         public StatusServiceTests()
         {
             _uowMock = new Mock<IUnitOfWork<Status>>();
-            _repoMock = new Mock<IRepository<Status>>();
+            _mapperMock = new Mock<IMapper>();
             _statusServiceMock = new Mock<StatusService>();
         }
 
         public async Task GetStatusesAsync_ReturnList()
         {
             // arrange
-            var result = Fixture.Build<Status>().CreateMany();
+            var entities = Fixture.Build<Status>().CreateMany();
+            var mapped = Fixture.Build<StatusDto>().CreateMany().ToList();
             var responseResult = Fixture.Create<ResponseResult>();
 
-            _repoMock.Setup(x => 
-                x.FindAsync(It.IsAny<Expression<Func<Status, bool>>>(), 
-                It.IsAny<IEnumerable<SortModel>>(), 
-                include: It.IsAny<IIncludableQueryable<Status ,Status>(), It.IsAny<bool>())).Returns(Task.FromResult(result));
+            _uowMock.Setup(x =>
+                x.Repository.FindAsync(It.IsAny<Expression<Func<Status, bool>>>(),
+                It.IsAny<IEnumerable<SortModel>>(), It.IsAny<Func<IQueryable<Status>, IIncludableQueryable<Status, object>>>(), It.IsAny<bool>())).Returns(Task.FromResult(entities));
+
+            _mapperMock.Setup(x => x.Map<IEnumerable<Status>, List<StatusDto>>(It.IsAny<IEnumerable<Status>>()))
+                .Returns(mapped);
+            // act
+            var result = await _statusServiceMock.Object.GetStatusesAsync();
+            var list = (List<StatusDto>)result.Data;
+            // assert
+            Assert.True(list.Any());
+            Assert.Equal(HttpStatusCode.OK , result.Status);
+            Assert.Null(result.Exception);
+            Assert.Equal("Success" , result.Message);
         }
     }
 }
