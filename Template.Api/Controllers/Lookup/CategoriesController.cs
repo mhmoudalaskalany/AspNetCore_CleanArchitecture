61524f6e8a9e079e5a78a2ed782d5d97f7290ffb
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2023-01-30 10:16:54 +0400

    add categories controller

---
+++ Template.Api/Controllers/Lookup/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Template.Api.Controllers.Base;
+using Template.Application.Services.Lookups.Category;
+using Template.Common.Core;
+
+namespace Template.Api.Controllers.Lookup
+{
+    /// <summary>
+    /// Actions Controller
+    /// </summary>
+    public class CategoriesController : BaseController
+    {
+        private readonly ICategoryService _service;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CategoriesController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+
+        /// <summary>
+        /// Get All Categories
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IFinalResult> GetCategoriesAsync() => await _service.GetCategoriesAsync();
+
+
+    }
+}
