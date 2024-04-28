cb7cf539deb4f5755d074c90099803cf3582b464
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2024-04-28 09:04:52 +0400

    modify v2 categories and actions controller

--- Template.Api/Controllers/V2/Lookup/CategoriesController.cs
+++ Template.Api/Controllers/V2/Lookup/CategoriesController.cs
@@ -17,44 +17,37 @@ namespace Template.Api.Controllers.V2.Lookup
     [Route("api/v{version:apiVersion}/[controller]")]
     public class CategoriesController : BaseController
     {
-        private readonly ICategoryService _service;
+        private readonly ICategoryService _categoryService;
         /// <summary>
         /// Constructor
         /// </summary>
-        public CategoriesController(ICategoryService service)
+        public CategoriesController(ICategoryService categoryService)
         {
-            _service = service;
+            _categoryService = categoryService;
         }
-
 
         /// <summary>
         /// Get By Id 
         /// </summary>
         /// <returns></returns>
         [HttpGet("get/{id}")]
-        public async Task<IFinalResult> GetAsync(int id)
-        {
-            var result = await _service.GetByIdAsync(id);
-            return result;
-        }
+        public async Task<IFinalResult> GetAsync(int id) => await _categoryService.GetByIdAsync(id);
+
 
         /// <summary>
         /// Get By Id For Edit 
         /// </summary>
         /// <returns></returns>
         [HttpGet("getEdit/{id}")]
-        public async Task<IFinalResult> GetEditAsync(int id)
-        {
-            var result = await _service.GetEditByIdAsync(id);
-            return result;
-        }
+        public async Task<IFinalResult> GetEditAsync(int id) => await _categoryService.GetEditByIdAsync(id);
+
 
         /// <summary>
         /// Get All Categories
         /// </summary>
         /// <returns></returns>
         [HttpGet("getAll")]
-        public async Task<IFinalResult> GetAllAsync() => await _service.GetAllAsync();
+        public async Task<IFinalResult> GetAllAsync() => await _categoryService.GetAllAsync();
 
 
         /// <summary>
@@ -63,10 +56,8 @@ namespace Template.Api.Controllers.V2.Lookup
         /// <param name="filter">Filter responsible for search and sort</param>
         /// <returns></returns>
         [HttpPost("getPaged")]
-        public async Task<DataPaging> GetPagedAsync([FromBody] BaseParam<CategoryFilter> filter)
-        {
-            return await _service.GetAllPagedAsync(filter);
-        }
+        public async Task<DataPaging> GetPagedAsync([FromBody] BaseParam<CategoryFilter> filter) => await _categoryService.GetAllPagedAsync(filter);
+
 
         /// <summary>
         /// Add 
@@ -74,11 +65,7 @@ namespace Template.Api.Controllers.V2.Lookup
         /// <param name="dto"></param>
         /// <returns></returns>
         [HttpPost("add")]
-        public async Task<IFinalResult> AddAsync([FromBody] AddCategoryDto dto)
-        {
-            var result = await _service.AddAsync(dto);
-            return result;
-        }
+        public async Task<IFinalResult> AddAsync([FromBody] AddCategoryDto dto) => await _categoryService.AddAsync(dto);
 
 
         /// <summary>
@@ -87,20 +74,16 @@ namespace Template.Api.Controllers.V2.Lookup
         /// <param name="model">Object content</param>
         /// <returns></returns>
         [HttpPut("update")]
-        public async Task<IFinalResult> UpdateAsync(AddCategoryDto model)
-        {
-            return await _service.UpdateAsync(model);
-        }
+        public async Task<IFinalResult> UpdateAsync(AddCategoryDto model) => await _categoryService.UpdateAsync(model);
+
         /// <summary>
         /// Remove  by id
         /// </summary>
         /// <param name="id">PK</param>
         /// <returns></returns>
         [HttpDelete("delete/{id}")]
-        public async Task<IFinalResult> DeleteAsync(int id)
-        {
-            return await _service.DeleteAsync(id);
-        }
+        public async Task<IFinalResult> DeleteAsync(int id) => await _categoryService.DeleteAsync(id);
+
 
         /// <summary>
         /// Soft Remove  by id
@@ -108,11 +91,7 @@ namespace Template.Api.Controllers.V2.Lookup
         /// <param name="id">PK</param>
         /// <returns></returns>
         [HttpDelete("deleteSoft/{id}")]
-        public async Task<IFinalResult> DeleteSoftAsync(int id)
-        {
-            return await _service.DeleteSoftAsync(id);
-        }
-
+        public async Task<IFinalResult> DeleteSoftAsync(int id) => await _categoryService.DeleteSoftAsync(id);
 
     }
 }
