98b613daa8328854304e184e85fc0c6adf7eb8ec
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2024-04-28 09:05:14 +0400

    modify v2 statuses controller

--- Template.Api/Controllers/V2/Lookup/StatusesController.cs
+++ Template.Api/Controllers/V2/Lookup/StatusesController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Template.Api.Controllers.V1.Base;
 using Template.Application.Services.Lookups.Status;
 using Template.Common.Core;
+using Template.Common.DTO.Base;
+using Template.Common.DTO.Lookup.Status.Parameters;
+using Template.Common.DTO.Lookup.Status;
 
 namespace Template.Api.Controllers.V2.Lookup
 {
@@ -14,20 +17,81 @@ namespace Template.Api.Controllers.V2.Lookup
     [Route("api/v{version:apiVersion}/[controller]")]
     public class StatusesController : BaseController
     {
-        private readonly IStatusService _service;
+        private readonly IStatusService _statusService;
         /// <summary>
         /// Constructor
         /// </summary>
-        public StatusesController(IStatusService service)
+        public StatusesController(IStatusService statusService)
         {
-            _service = service;
+            _statusService = statusService;
         }
 
+
         /// <summary>
-        /// Get All Statuses
+        /// Get By Id 
         /// </summary>
         /// <returns></returns>
-        [HttpGet("getStatuses")]
-        public async Task<IFinalResult> GetStatusesAsync() => await _service.GetStatusesAsync();
+        [HttpGet("get/{id}")]
+        public async Task<IFinalResult> GetAsync(int id) => await _statusService.GetByIdAsync(id);
+
+
+        /// <summary>
+        /// Get By Id For Edit 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("getEdit/{id}")]
+        public async Task<IFinalResult> GetEditAsync(int id) => await _statusService.GetEditByIdAsync(id);
+
+
+        /// <summary>
+        /// Get All Categories
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("getAll")]
+        public async Task<IFinalResult> GetAllAsync() => await _statusService.GetAllAsync();
+
+
+        /// <summary>
+        /// Get Paged
+        /// </summary>
+        /// <param name="filter">Filter responsible for search and sort</param>
+        /// <returns></returns>
+        [HttpPost("getPaged")]
+        public async Task<DataPaging> GetPagedAsync([FromBody] BaseParam<StatusFilter> filter) => await _statusService.GetAllPagedAsync(filter);
+
+
+        /// <summary>
+        /// Add 
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost("add")]
+        public async Task<IFinalResult> AddAsync([FromBody] AddStatusDto dto) => await _statusService.AddAsync(dto);
+
+
+        /// <summary>
+        /// Update  
+        /// </summary>
+        /// <param name="model">Object content</param>
+        /// <returns></returns>
+        [HttpPut("update")]
+        public async Task<IFinalResult> UpdateAsync(AddStatusDto model) => await _statusService.UpdateAsync(model);
+
+        /// <summary>
+        /// Remove  by id
+        /// </summary>
+        /// <param name="id">PK</param>
+        /// <returns></returns>
+        [HttpDelete("delete/{id}")]
+        public async Task<IFinalResult> DeleteAsync(int id) => await _statusService.DeleteAsync(id);
+
+
+        /// <summary>
+        /// Soft Remove  by id
+        /// </summary>
+        /// <param name="id">PK</param>
+        /// <returns></returns>
+        [HttpDelete("deleteSoft/{id}")]
+        public async Task<IFinalResult> DeleteSoftAsync(int id) => await _statusService.DeleteSoftAsync(id);
     }
 }
